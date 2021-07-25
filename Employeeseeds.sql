3ef60492367cd03869980fbd59993de13eaad620
Anjeh Mbah <junez24@yahoo.com>
2021-07-25 19:57:53 -0400

    adding employee seeds

--- Employeeseeds.sql
+++ Employeeseeds.sql
@@ -15,9 +15,20 @@ VALUE ("Safety");
 
 INSERT INTO role (department_id, title, salary)
 VALUES (1, "HR-Manager", 75000),
-       (1, "HR-Assistant", 5000),
-       (2, "Upper leadership", 8000),
+       (1, "HR-Assistant", 50000),
+       (2, "Upper leadership", 80000),
        (2, "Supervisor", 45000),
-       (3, "Payroll-personnel", 5000),
+       (3, "Payroll-personnel", 50000),
        (4, "Safety personnel I", 55000),
-       (4, "Safety personnel II", 5000);
+       (4, "Safety personnel II", 50000);
+
+--Mapping out seeds for employee table--
+
+INSERT INTO employee (role_id, manager_id, first_name, last_name)
+VALUES (1, null, "Ishya", "Cas"),
+        (1, null, "Jenn", "Good"),
+        (2, null, "Anjeh", "M."),
+        (2, null, "Ange", "Lowe"),
+        (3, null, "Anam", "Braz"),
+        (4, null, "Bruno", "Calder"),
+        (4, null, "Marie", "Rae");
