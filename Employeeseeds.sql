81a6ee452ba84d1d65b62c26c06e3d4f6d430265
Anjeh Mbah <junez24@yahoo.com>
2021-07-25 19:31:30 -0400

    inserting seeds into role

--- Employeeseeds.sql
+++ Employeeseeds.sql
@@ -10,3 +10,14 @@ INSERT INTO department (name)
 VALUE ("Payroll");
 INSERT INTO department (name)
 VALUE ("Safety");
+
+--Mapping out seeds for various roles--
+
+INSERT INTO role (department_id, title, salary)
+VALUES (1, "HR-Manager", 75000),
+       (1, "HR-Assistant", 5000),
+       (2, "Upper leadership", 8000),
+       (2, "Supervisor", 45000),
+       (3, "Payroll-personnel", 5000),
+       (4, "Safety personnel I", 55000),
+       (4, "Safety personnel II", 5000),
