711cc090f59a8df6d210c32b1136b7fa114e910b
Anjeh Mbah <junez24@yahoo.com>
2021-07-25 18:58:10 -0400

    Adding seeds /names of different deparments

---
+++ Employeeseeds.sql
@@ -0,0 +1,12 @@
+USE employeetracker_db;
+
+--Mapping out seeds / data for the dapartment table--
+
+INSERT INTO department (name)
+VALUE ("Human Resources");
+INSERT INTO department (name)
+VALUE ("Leadership");
+INSERT INTO department (name)
+VALUE ("Payroll");
+INSERT INTO department (name)
+VALUE ("Safety");
